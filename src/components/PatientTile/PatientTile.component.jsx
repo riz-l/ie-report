71e8d65c3a4a6d403e3acb5d99e20bc79e98fc7f
Riz <joshua.riz.layton@gmail.com>
2020-11-18 17:06:30 +0000

    IEREP-49: Adding API calls through axios, and an axios.js file

--- src/components/PatientTile/PatientTile.component.jsx
+++ src/components/PatientTile/PatientTile.component.jsx
@@ -1,5 +1,6 @@
 // Import: Dependencies
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { patientDetails } from "../../utils/axios";
 
 // Import: Elements
 import {
@@ -20,48 +21,128 @@ import { FieldData, HeadingPrimary, ReportContainer } from "../index";
 
 // Component: PatientTile
 export default function PatientTile() {
-  return (
-    <Container>
-      <Section>
-        <Heading>
-          <HeadingPrimary
-            icon="fas fa-hospital-user"
-            text="Patient Demographics"
-            padding="0.6rem"
-          />
-        </Heading>
+  // State = patientData
+  const [patientData, setPatientData] = useState([]);
 
-        <ReportContainer>
-          <Render>
-            <PatientHeading>
-              <h2>Forename</h2>
-              <h2>Surname</h2>
-            </PatientHeading>
+  // Patient Master_ePR_ID
+  const patientId = "d24fdd2c-021f-4329-9449-34cea4ee3503";
 
-            <HorizontalRule />
+  // Fetch Patient Data
+  function getPatientData() {
+    patientDetails
+      .get(`/${patientId}`, {})
+      .then((res) => {
+        const data = res.data;
+        console.log("Patient Data: ", data);
+        setPatientData(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
-            <Grid>
-              <ColumnOne>
-                <FieldData field="Date of Birth" data="Test Data" />
-                <FieldData field="Age (Years)" data="Test Data" />
-                <FieldData field="Age (Months)" data="Test Data" />
-              </ColumnOne>
+  // Run getPatientData on load
+  useEffect(() => {
+    getPatientData();
+  }, []);
 
-              <ColumnTwo>
-                <FieldData field="Gender" data="Test Data" />
-                <FieldData field="Ethnicity" data="Test Data" />
-                <FieldData field="Weight (kg)" data="Test Data" />
-              </ColumnTwo>
+  //#region patientRender = Patient Demographics
+  const patientRender = patientData.map((patient) => (
+    <Section>
+      <Heading>
+        <HeadingPrimary
+          icon="fas fa-hospital-user"
+          text="Patient Demographics"
+          padding="0.6rem"
+        />
+      </Heading>
 
-              <ColumnThree>
-                <FieldData field="NHS No." data="Test Data" />
-                <FieldData field="Telephone" data="Test Data" />
-                <FieldData field="Address" data="Test Data" />
-              </ColumnThree>
-            </Grid>
-          </Render>
-        </ReportContainer>
-      </Section>
-    </Container>
-  );
+      <ReportContainer>
+        <Render>
+          <PatientHeading>
+            <h2>{patient.PD_Firstname ? patient.PD_Firstname : "Firstname"}</h2>
+            <h2>{patient.PD_Surname ? patient.PD_Surname : "Surname"}</h2>
+          </PatientHeading>
+
+          <HorizontalRule />
+
+          <Grid>
+            <ColumnOne>
+              <FieldData
+                field="Date of Birth"
+                data={patient.PD_DOB ? patient.PD_DOB : "Not Recorded"}
+              />
+              <FieldData
+                field="Age (Years)"
+                data={patient.PD_Age_Yrs ? patient.PD_Age_Yrs : "Not Recorded"}
+              />
+              <FieldData
+                field="Age (Months)"
+                data={
+                  patient.PD_Age_Mths ? patient.PD_Age_Mths : "Not Recorded"
+                }
+              />
+            </ColumnOne>
+
+            <ColumnTwo>
+              <FieldData
+                field="Gender"
+                data={patient.PD_Gender ? patient.PD_Gender : "Not Recorded"}
+              />
+              <FieldData
+                field="Ethnicity"
+                data={
+                  patient.PD_Ethnicity ? patient.PD_Ethnicity : "Not Recorded"
+                }
+              />
+              <FieldData
+                field="Weight (kg)"
+                data={patient.PD_Weight ? patient.PD_Weight : "Not Recorded"}
+              />
+            </ColumnTwo>
+
+            <ColumnThree>
+              <FieldData
+                field="NHS No."
+                data={patient.PD_Nhs_No ? patient.PD_Nhs_No : "Not Recorded"}
+              />
+              <FieldData
+                field="Telephone"
+                data={
+                  patient.PD_Telephone ? patient.PD_Telephone : "Not Recorded"
+                }
+              />
+              <FieldData
+                field="Address"
+                data={
+                  patient.PD_Housenum ||
+                  patient.PD_Add1 ||
+                  patient.PD_Add2 ||
+                  patient.PD_Add3 ||
+                  patient.PD_Postcode ? (
+                    <>
+                      {patient.PD_Housenum}
+                      <br />
+                      {patient.PD_Add1}
+                      <br />
+                      {patient.PD_Add2}
+                      <br />
+                      {patient.PD_Add3}
+                      <br />
+                      {patient.PD_Postcode}
+                    </>
+                  ) : (
+                    "Not recorded"
+                  )
+                }
+              />
+            </ColumnThree>
+          </Grid>
+        </Render>
+      </ReportContainer>
+    </Section>
+  ));
+  //#endregion /patientRender = Patient Demographics
+
+  return <Container>{patientRender}</Container>;
 }
